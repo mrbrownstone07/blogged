1cddb7b17eb63a311d02c1e58466b296152706e8
mrbrownstone07 <mahdi.mad07@gmail.com>
2018-11-30 01:51:22 +0600

    currently working on PagesController for the global pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        return view('pages.index');
+    }
+
+    public function about_us(){
+        return view('pages.about');
+    }
+
+    public function contact_us(){
+        return view('pages.contact');
+    }
+
+    public function faq(){
+        return view('pages.faq');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index');
+
+// Route::get('/about', function(){
+//     return view('pages.about');
+// });
